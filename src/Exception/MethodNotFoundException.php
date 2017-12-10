549133e1766aff201cda5cecb096b1d359a0b2d8
jdomenechb <jdomenechbborges@gmail.com>
2017-12-10 02:06:39 +0100

    Added MethodNotFoundException

---
+++ src/Exception/MethodNotFoundException.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * This file is part of the brchain package.
+ *
+ * (c) Jordi Domènech Bonilla
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Jdomenechb\BRChain\Exception;
+use Throwable;
+
+/**
+ * Exception thrown when a method has not been found.
+ * @package Jdomenechb\BRChain\Exception
+ */
+class MethodNotFoundException extends BRChainException
+{
+    /**
+     * MethodNotFoundException constructor.
+     * @param string $methodName
+     * @param string $className
+     * @param Throwable|null $previous
+     */
+    public function __construct(string $methodName, string $className, Throwable $previous = null)
+    {
+        $msg = 'The method "' . $methodName . '" has not been found in "' . $className . '"';
+        parent::__construct($msg, 0, $previous);
+    }
+}
