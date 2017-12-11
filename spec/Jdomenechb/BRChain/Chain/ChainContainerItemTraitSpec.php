39085c441a6f6a7ef9013d871ae23bb16ad34f67
jdomenechb <jdomenechbborges@gmail.com>
2017-12-12 00:41:39 +0100

    Added ChainContainerItemTrait spec

---
+++ spec/Jdomenechb/BRChain/Chain/ChainContainerItemTraitSpec.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * This file is part of the brchain package.
+ *
+ * (c) Jordi Domènech Bonilla
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace spec\Jdomenechb\BRChain\Chain;
+
+
+use Jdomenechb\BRChain\Chain\ChainInterface;
+use Jdomenechb\BRChain\Stub\Chain\ChainContainerItemTraitStub;
+use Jdomenechb\BRChain\Test\ObjectBehavior;
+
+class ChainContainerItemTraitSpec extends ObjectBehavior
+{
+    public function let()
+    {
+        $this->beAnInstanceOf(ChainContainerItemTraitStub::class);
+    }
+
+    public function it_can_receive_a_Chain(ChainInterface $chain)
+    {
+        $this->setChain($chain);
+        $this->getChain()->shouldBe($chain);
+    }
+
+    public function it_can_create_lazily_a_Chain()
+    {
+        $this->getChain()->shouldReturnAnInstanceOf(ChainInterface::class);
+    }
+}
---
+++ stubs/Chain/ChainContainerItemTraitStub.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This file is part of the brchain package.
+ *
+ * (c) Jordi Domènech Bonilla
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Jdomenechb\BRChain\Stub\Chain;
+
+
+use Jdomenechb\BRChain\Chain\ChainContainerItemTrait;
+
+/**
+ * Stub for testing ChainContainerItemTrait.
+ * @package Jdomenechb\BRChain\Stub\Chain
+ */
+class ChainContainerItemTraitStub
+{
+    use ChainContainerItemTrait;
+}
