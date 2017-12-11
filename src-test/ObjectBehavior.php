ccddb8174c6ea461ff4f5ce574c4c6ba7a3c9726
jdomenechb <jdomenechbborges@gmail.com>
2017-12-12 00:40:42 +0100

    Added improved ObjectBehavior

---
+++ src-test/ObjectBehavior.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * This file is part of the brchain package.
+ *
+ * (c) Jordi Domènech Bonilla
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Jdomenechb\BRChain\Test;
+
+/**
+ * Class extending the default ObjectBehaviour to provide more matchers.
+ * @package Jdomenechb\BRChain\Test
+ */
+class ObjectBehavior extends \PhpSpec\ObjectBehavior
+{
+    /**
+     * Extended matchers to provide to the spec.
+     * @return array
+     */
+    public function getMatchers(): array
+    {
+        return [
+            /*
+             * Checks if the class or its parents use an specific trait
+             */
+            'useTrait' => function ($subject, $traitName) {
+                $className = \get_class($subject);
+                $uses = \class_uses(\get_class($subject));
+
+                if (\in_array($traitName, $uses, true)) {
+                    return true;
+                }
+
+                $parents = class_parents($className);
+
+                foreach ($parents as $parent) {
+                    $uses = \class_uses($parent);
+
+                    if (\in_array($traitName, $uses, true)) {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+        ];
+    }
+}
