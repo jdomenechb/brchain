4e99e71db246a2471ec0003084742ed9baee46ab
jdomenechb <jdomenechbborges@gmail.com>
2018-01-16 00:16:52 +0100

    Added Spec for AbstractString

---
+++ spec/Jdomenechb/BRChain/String/AbstractStringSpec.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace spec\Jdomenechb\BRChain\String;
+
+use Jdomenechb\BRChain\CallStringOptionTrait;
+use Jdomenechb\BRChain\DynamicOptionsTrait;
+use Jdomenechb\BRChain\PropertyItemInterface;
+use Jdomenechb\BRChain\String\AbstractString;
+use Jdomenechb\BRChain\String\StringInterface;
+use Jdomenechb\BRChain\Stub\String\AbstractStringStub;
+use Jdomenechb\BRChain\Test\ObjectBehavior;
+
+class AbstractStringSpec extends ObjectBehavior
+{
+    public function let()
+    {
+        $this->beAnInstanceOf(AbstractStringStub::class);
+    }
+
+    public function its_stub_extends_the_abstract_class()
+    {
+        $this->shouldHaveType(AbstractString::class);
+    }
+
+    public function it_is_a_String_item()
+    {
+        $this->shouldImplement(StringInterface::class);
+    }
+
+    public function it_is_a_property_item()
+    {
+        $this->shouldImplement(PropertyItemInterface::class);
+    }
+
+    public function it_accepts_dynamic_options()
+    {
+        $this->shouldUseTrait(DynamicOptionsTrait::class);
+    }
+
+    public function it_can_return_properties_as_strings()
+    {
+        $this->shouldUseTrait(CallStringOptionTrait::class);
+    }
+}
---
+++ stubs/String/AbstractStringStub.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the brchain package.
+ *
+ * (c) Jordi Domènech Bonilla
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Jdomenechb\BRChain\Stub\String;
+
+use Jdomenechb\BRChain\Condition\AbstractCondition;
+use Jdomenechb\BRChain\SourceItem\SourceItemInterface;
+use Jdomenechb\BRChain\String\AbstractString;
+
+/**
+ * Stub class for checking AbstractString.
+ */
+class AbstractStringStub extends AbstractString
+{
+    /**
+     * @inheritdoc
+     */
+    public function __toString()
+    {
+        return '';
+    }
+}
